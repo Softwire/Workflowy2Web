61b82bed197a47cdc218950f8a87f361290eed8a
Sarah Thurneyssen <sarah.thurneyssen@softwire.com>
2015-08-17 15:28:54 +0100

    Moved src and lib files into their own folders

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, { file: "startContentProcessor.js" });
+  chrome.tabs.executeScript(null, { file: "src/startContentProcessor.js" });
 });
