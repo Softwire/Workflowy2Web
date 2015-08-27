610f1157ca90a25a45175ac861a6b487dc9674d6
Sarah Thurneyssen <sarah.thurneyssen@softwire.com>
2015-08-27 14:46:27 +0100

    Changed how the export pop up is triggered so entire tree is not exported

--- src/startContentProcessor.js
+++ src/startContentProcessor.js
@@ -1,12 +1,17 @@
-﻿// Dependent on DOM structure of workflowy
-$('#exportAllButton').click();
+﻿// Dependent on DOM structure and javascript of workflowy
+// exportIt is a custom workflowy function so can only be called in the page context
+var script = document.createElement('script');
+var code = document.createTextNode('(function() {$(".project.selected").exportIt();})();');
+script.appendChild(code);
+document.head.appendChild(script);
+
 var opml = $('#exportPopup').find('.opmlContainer').text();
 $('#exportPopup').parent().find('.ui-icon-closethick').click();
 
 var parsedOpml = $.parseXML(opml);
 var xml = $(parsedOpml);
 
-var siteTitle = $('.mainTreeRoot > .name').text();
+var siteTitle = $('.project.selected > .name').text();
 
 var converter = new Converter(xml, siteTitle);
 converter.GetZippedHtmlFiles(function (data) {
