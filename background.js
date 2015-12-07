e9c54035a0d635768b9e034dec04f82c15d35b2e
Samuel.Neugber <samuel.neugber@softwire.com>
2015-12-07 15:33:30 +0000

    Showing the converter button in the address bar only on workflowy pages

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.pageAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, { file: "src/startContentProcessor.js" });
+});
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'workflowy.com', schemes: ['https', 'http'] },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
 });
